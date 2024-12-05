89fbdc2b79288e7e82961e9db93c2da93813d3b6
rocknroll17 <gon1397@naver.com>
2024-12-05 16:26:26 +0900

    feat:Added parsing password and printing password

--- Windows.py
+++ Windows.py
@@ -9,12 +9,12 @@ class InformationNotFound(Exception):
         super().__init__(self.message)
 
 class windows:
+    keywords = ["CCMP", "GCMP", "WEP", "TKIP", "WPA3", "WPA2", "WPA"]
     def main(self):
         profiles = self.Windows_get_wifi_profiles()
         for profile in profiles:
-            self.Windows_get_wifi_password(profile)
-            # print(f"Profile: {profile}\nPassword: {password}\n\n")
-            # break
+            password = self.Windows_get_wifi_password(profile)
+            print(f"Profile: {profile}\nPassword: {password}\n")
 
     def Windows_get_wifi_profiles(self):
         profiles_output = subprocess.check_output("netsh wlan show profiles", shell=True, encoding='utf-8')
@@ -26,19 +26,16 @@ class windows:
         try:
             profile_output = subprocess.check_output(f'netsh wlan show profile name="{profile}" key=clear', shell=True, encoding='utf-8', env=dict(os.environ, LANG='en_US.UTF-8'))
             matches = re.findall(r'[^\n]+\n-{5,}[^\n]*\n(?:[^\n]+\n)*\n', profile_output)
-            keywords = ["CCMP", "GCMP", "WEP", "TKIP", "WPA3", "WPA2", "WPA"]
             count = Counter()
             max_index = None
             for i, match in enumerate(matches):
-                count[i] = sum(match.count(keyword) for keyword in keywords if keyword in match)
+                count[i] = sum(match.count(keyword) for keyword in windows.keywords if keyword in match)
             if count and all(value == 0 for value in count.values()):
                 return None
             else:
                 max_index = max(count, key=count.get)
                 password_paragraph = matches[max_index]
-            # print(max_index)
-            # print(count)
-            # print(f"Profile: {profile}")
-            print(password_paragraph)
+            password = re.findall(r'[^\n]+\n', password_paragraph)[-1].split(":")[-1].strip()
+            return password
         except subprocess.CalledProcessError:
             return "[Access Denied]"
